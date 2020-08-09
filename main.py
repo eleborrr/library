326b1e001bed0f9075410d4ce6934ca7eca5ccd3
esrev <gleb-petuhov@mail.ru>
2020-08-09 21:25:50 +0300

    start

--- main.py
+++ main.py
@@ -8,18 +8,9 @@ class StartWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.setWindowTitle("Библиотека")
-        self.con = sqlite3.connect("library.db")
-        self.cur = self.con.cursor()
-        self.loadTable()
 
     def loadTable(self):
-        result = self.cur.execute("SELECT * FROM Books").fetchall()
-        self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setColumnCount(len(result[0]))
-        for i, elem in enumerate(result):
-            for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        pass
 
     def add_book(self):
         pass
