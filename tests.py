7e1805feae9db39124d41802501e19ed1b5aaf1f
TimurTimergalin <timur20041229@gmail.com>
2020-08-10 12:39:40 +0300

    existing functions are tested

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import create_roles, lend_book, return_book, register_user
+from main import create_roles, lend_book, return_book, register_user, create_library, add_edition
 from data import db_session, role, user, edition, library, book
 import unittest
 import os
@@ -27,6 +27,102 @@ class AppTests(unittest.TestCase):
         register_user('name', 'surname', 'login', 'password', 1, lib.id, 7)
         register_user('name', 'surname', 'login', 'password', 1, lib.id, 7)
         self.assertEqual(len(session.query(user.User).all()), 1)
+        session.close()
+
+    def test_create_library(self):
+        create_library('School Name',
+                       login='login',
+                       name='name',
+                       surname='surname',
+                       password='password',
+                       class_num=None)
+        session = db_session.create_session()
+        librarian = session.query(user.User).first()
+        self.assertIsNotNone(librarian)
+        self.assertEqual(librarian.role_id, 2)
+        self.assertEqual(librarian.library_id, 1)
+        self.assertIsNotNone(session.query(library.Library).first())
+        session.close()
+
+    def test_add_edition(self):
+        create_library('School Name',
+                       login='login',
+                       name='name',
+                       surname='surname',
+                       password='password',
+                       class_num=None)
+        session = db_session.create_session()
+        lib = session.query(library.Library).first()
+        add_edition(lib.id, 4,
+                    name='name',
+                    author='author',
+                    publication_year=1984)
+        ed = session.query(edition.Edition).first()
+        self.assertIsNotNone(ed)
+        books = session.query(book.Book).all()
+        self.assertEqual(len(books), 4)
+        for i in books:
+            self.assertEqual(i.edition_id, ed.id)
+        session.close()
+
+    def test_lend_book(self):
+        create_library('School Name',
+                       login='login',
+                       name='name',
+                       surname='surname',
+                       password='password',
+                       class_num=None)
+        session = db_session.create_session()
+        lib = session.query(library.Library).first()
+        add_edition(lib.id, 4,
+                    name='name',
+                    author='author',
+                    publication_year=1984)
+        register_user('name', 'surname', 'login', 'password', 1, lib.id, 7)
+        register_user('name', 'surname', 'login1', 'password', 1, lib.id, 7)
+        user1 = session.query(user.User).get(1)
+        user2 = session.query(user.User).get(2)
+        self.assertIsNotNone(user1)
+        self.assertIsNotNone(user2)
+        lend_book(user1.id, 1)
+        lend_book(user2.id, 3)
+        book1 = session.query(book.Book).get(1)
+        book2 = session.query(book.Book).get(2)
+        book3 = session.query(book.Book).get(3)
+        book4 = session.query(book.Book).get(4)
+        self.assertIsNotNone(book1)
+        self.assertIsNotNone(book2)
+        self.assertIsNotNone(book3)
+        self.assertIsNotNone(book4)
+        self.assertEqual(book1.owner_id, user1.id, 1)
+        self.assertEqual(book2.owner_id, None, 2)
+        self.assertEqual(book3.owner_id, user2.id, 3)
+        self.assertEqual(book4.owner_id, None, 4)
+        session.close()
+
+    def test_return_book(self):
+        create_library('School Name',
+                       login='login',
+                       name='name',
+                       surname='surname',
+                       password='password',
+                       class_num=None)
+        session = db_session.create_session()
+        lib = session.query(library.Library).first()
+        add_edition(lib.id, 4,
+                    name='name',
+                    author='author',
+                    publication_year=1984)
+        register_user('name', 'surname', 'login', 'password', 1, lib.id, 7)
+        register_user('name', 'surname', 'login1', 'password', 1, lib.id, 7)
+        user1 = session.query(user.User).get(1)
+        user2 = session.query(user.User).get(2)
+        lend_book(user1.id, 1)
+        lend_book(user2.id, 3)
+        for i in range(1, 5):
+            return_book(i)
+        for i in session.query(book.Book).all():
+            self.assertIsNone(i.owner_id)
         session.close()
 
     def tearDown(self) -> None:
