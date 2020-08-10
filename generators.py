32df399f9f5a3c4c508901e0f3b2e2d7a9ab028f
TimurTimergalin <timur20041229@gmail.com>
2020-08-10 19:32:17 +0300

    create_qrcode fixed

--- generators.py
+++ generators.py
@@ -4,11 +4,11 @@ from data.user import User
 import qrcode
 
 
-def create_qrcode(library_id, book_id):
-    img = qrcode.make(f'http://mylibby.ru/{library_id}/{book_id}')
+def create_qrcode(book_id):
+    img = qrcode.make(f'http://mylibby.ru/{book_id}')  # Книга привязана к библиотеке, нет смысла в доп. аргументе
     filename = f'{book_id}.png'
     img.save(filename)
 
 
 if __name__ == '__main__':
-    create_qrcode(1, 2)
+    create_qrcode(1)
